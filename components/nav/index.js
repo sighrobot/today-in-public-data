1a1b45e6c25a11cb7d7897f6c4bf745b0d593ca6
Abe Rubenstein <abe.rubenstein@enigma.com>
2018-06-19 17:10:32 -0700

    Externalize nav styles

--- components/nav/index.js
+++ components/nav/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import moment from 'moment';
 
-import Calendar from './calendar';
-import DatePicker from './date-picker';
-import {isToday} from '../lib/utils';
+import Calendar from '../calendar';
+import DatePicker from '../date-picker';
+import { isToday } from '../../lib/utils';
+
+import style, { triggerStyle } from './style';
 
 export default class Nav extends React.PureComponent {
   state = {
@@ -29,28 +31,7 @@ export default class Nav extends React.PureComponent {
           <strong>{this.renderDateText()}</strong> in&nbsp;Public&nbsp;Data
         </span>
 
-        <style jsx>{`
-          .date-select-trigger {
-            border: none;
-            background: transparent;
-            color: white;
-            font-size: 16px;
-            outline: 0;
-            padding: 16px 40px;
-            text-align: center;
-            width: 100%;
-          }
-
-          span {
-           border-bottom: 1px dotted white;
-           transition: 200ms ease text-shadow;
-          }
-
-          span:hover {
-            cursor: pointer;
-            text-shadow: 5px 5px 1px rgba(255, 0, 0, 0.5), -5px -5px 1px rgba(0, 0, 255, 0.5);
-          }
-        `}</style>
+        <style jsx>{ triggerStyle }</style>
       </button>
     );
   }
@@ -78,14 +59,7 @@ export default class Nav extends React.PureComponent {
 
         {this.renderTitle()}
 
-        <style jsx>{`
-          nav {
-            background: linear-gradient(to right, #0063B2, #DC143C);
-            color: white;
-            text-align: center;
-            box-shadow: 0px 3px 5px rgba(0,0, 0, 0.5);
-          }
-        `}</style>
+        <style jsx>{ style }</style>
       </nav>
     );
   }
