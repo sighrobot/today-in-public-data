cb012d8468449ae9ee5c22c2d88b901d3e2504fb
Abe Rubenstein <sighrobot@gmail.com>
2020-02-01 18:04:57 -0500

    Minor reskin and add version to footer

--- components/footer/index.js
+++ components/footer/index.js
@@ -2,12 +2,20 @@ import React from 'react'
 
 import './style.styl'
 
+const p = require('../../package.json')
+
 export default class Footer extends React.PureComponent {
   render() {
     return (
       <footer>
         <div>
-          <strong>Made with public and open data</strong> by{' '}
+          <a
+            href={`${p.repository}/releases/tag/v${p.version}`}
+            target="_blank"
+          >
+            v{p.version}
+          </a>{' '}
+          <strong>made with public and open data</strong> by{' '}
           <a
             className="outbound"
             target="_blank"
