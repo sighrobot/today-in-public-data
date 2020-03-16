1a23e37c2d471b2737c2514627b6501176f3e854
Abe Rubenstein <abe.rubenstein@datadoghq.com>
2020-03-16 17:06:52 -0400

    Configure for now deployments

--- next.config.js
+++ next.config.js
@@ -1,16 +1,4 @@
-const webpack = require('webpack')
 const withCSS = require('@zeit/next-css')
 const withStylus = require('@zeit/next-stylus')
 
-module.exports = withStylus(
-  withCSS({
-    webpack: config => {
-      // Fixes npm packages that depend on `fs` module
-      config.node = {
-        fs: 'empty',
-      }
-
-      return config
-    },
-  })
-)
+module.exports = withStylus(withCSS())
